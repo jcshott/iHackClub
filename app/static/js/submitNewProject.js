9151141cdceafd7e487b364bd62fc976f74382c0
Corey Shott <jcshott@gmail.com>
2016-04-08 12:41:11 -0700

    started validation

---
+++ app/static/js/submitNewProject.js
@@ -0,0 +1,37 @@
+// default the entry in start date input to today
+$("#projectStart").val(
+    function(){
+      return new Date().toISOString().split('T')[0]
+    }
+);
+
+// change the carets when collapse is hidden/shown
+$('#pg').on('hide.bs.collapse', function (e) {
+  var target = e.target.id;
+  var headerId = document.getElementById(target).getAttribute("aria-labelledby");
+  var selector = "#" + headerId
+  $(selector).find("span").attr("class", "glyphicon glyphicon-triangle-right");
+
+});
+
+$('#pg').on('show.bs.collapse', function (e) {
+  var target = e.target.id;
+  var headerId = document.getElementById(target).getAttribute("aria-labelledby");
+  var selector = "#" + headerId
+  $(selector).find("span").attr("class", "glyphicon glyphicon-triangle-bottom");
+
+});
+
+// submission of form
+$('#newProjectForm').validator().on('submit', function (evt) {
+  if (evt.isDefaultPrevented()) {
+    // here's were logic goes if form is invalid
+  } else {
+    evt.preventDefault();
+	// successfully submits data to route. route successfully parses into dictionary
+		$.post("/save_project", $( "#newProjectForm" ).serialize(), function (result){
+			console.log(result);
+		});
+
+  }
+})
